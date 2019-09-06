3fb24469d31bda0c32f29255fb128b16a010b7a6
christian williams <christian.williamsj@gmail.com>
2019-09-06 06:03:50 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -18,3 +18,11 @@ def take_a_number(array, name)
   puts "Wecome #{name}, you are #{array.index(name)+1} in line."
   return name, position
 end
+
+def now_serving(array)
+  if array.empty? == true 
+    puts "There is nobody waiting to be served!"
+  elsif array.empty? == false 
+    puts "Currently serving #{array.shift}."
+  end 
+end 
